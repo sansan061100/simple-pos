d8be28542cd1bfb459fa0f7dac9dc69d285679ac
Sansan <sansan001106@gmail.com>
2023-04-10 07:08:14 +0700

    tambah widdget di dashboard

--- app/Helper.php
+++ app/Helper.php
@@ -1,9 +1,9 @@
 <?php
 
-function rupiah($angka, $prefix = 'Rp ')
+function rupiah($number, $prefix = 'Rp ')
 {
-    $hasil_rupiah = $prefix . number_format($angka, 0, '', '.');
-    return $hasil_rupiah;
+    $rupiah = $prefix . number_format($number, 0, '', '.');
+    return $rupiah;
 }
 
 function getUser()
@@ -14,4 +14,38 @@ function getUser()
 function dateFormater($date, $format = 'd/m/Y, H:i:s')
 {
     return Carbon\Carbon::parse($date)->format($format);
+}
+
+function allMonths()
+{
+    return [
+        '01' => 'January',
+        '02' => 'February',
+        '03' => 'March',
+        '04' => 'April',
+        '05' => 'May',
+        '06' => 'June',
+        '07' => 'July',
+        '08' => 'August',
+        '09' => 'September',
+        '10' => 'October',
+        '11' => 'November',
+        '12' => 'December',
+    ];
+}
+
+function allYears($year = 2023, $previous = 0, $next = 0)
+{
+    $years = [];
+    for ($i = $year; $i >= $year - $previous; $i--) {
+        $years[$i] = $i;
+    }
+
+    for ($i = $year + 1; $i <= $year + $next; $i++) {
+        $years[$i] = $i;
+    }
+
+    // sort by key
+    ksort($years);
+    return $years;
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,1 +1,86 @@
 @extends('admin.layouts.app')
+
+@section('content')
+    <div class="col-md-12">
+        <form class="card">
+            <div class="card-header">
+                <div class="row">
+                    <div class="form-group col-md-3">
+                        <label>Month</label>
+                        <select name="month" class="select2 w-100">
+                            <option value="">Month</option>
+                            @foreach (allMonths() as $key => $month)
+                                <option value="{{ $key }}">
+                                    {{ $month }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group col-md-3">
+                        <label>Year</label>
+                        <select name="year" class="select2 w-100">
+                            <option value="">Year</option>
+                            @foreach (allYears(2023, 5, 5) as $year)
+                                <option value="{{ $year }}">
+                                    {{ $year }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class="form-group col-md-2">
+                        <label>&nbsp;</label>
+                        <button class="btn btn-primary btn-block">Filter</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">User</span>
+                <span class="info-box-number">1,410</span>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Product</span>
+                <span class="info-box-number">1,410</span>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Customer</span>
+                <span class="info-box-number">1,410</span>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6 col-12">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="far fa-envelope"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Order</span>
+                <span class="info-box-number">1,410</span>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('styles')
+    @include('admin.plugins.select2-css')
+@endpush
+
+@push('scripts')
+    @include('admin.plugins.select2-js')
+    <script>
+        $('.select2').select2({
+            theme: 'bootstrap4',
+        });
+    </script>
+@endpush
